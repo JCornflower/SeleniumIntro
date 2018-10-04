36014cbe9a5646fc5864a4bea231d10aca03cb6b
JCornflower <31407200+JCornflower@users.noreply.github.com>
2018-10-04 16:02:50 +0300

    edited1

--- test.js
+++ test.js
@@ -1,12 +1,14 @@
 var webdriver = require('selenium-webdriver');
 var By = webdriver.By;
-var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+var driver = new webdriver.Builder().forBrowser('chrome').build();
 
 
 
 driver.get('https://rozetka.com.ua');
+//driver.manage().window().fullscreen();
+
 driver.findElement(By.name('signin')).click();
-driver.switchTo().alert().sendKeys('ol2007july@gmail.com');
+
 
 
 
