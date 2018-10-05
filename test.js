181ce79d768ebdd60888a7c2d11cbfaea9628dab
JCornflower <31407200+JCornflower@users.noreply.github.com>
2018-10-05 15:37:45 +0300

    back

--- test.js
+++ test.js
@@ -7,10 +7,26 @@ var driver = new webdriver.Builder().forBrowser('chrome').build();
 driver.get('https://rozetka.com.ua');
 //driver.manage().window().fullscreen();
 
+// driver.findElement(By.name('signin')).click();
+// driver.wait(function() {
+//     console.error('wait');
+//     return driver.elementLocated(webdriver.By.xpath('//*[@id="user-popup-parent59321"]/div/div'));
+// }, 10000);
+// //driver.findElement(By.xpath('//*[@id="user-popup-parent59321"]/div/div')).isSelected();
+// driver.findElement(By.css('input')).sendKeys('ol2007july@gmail.com');
+
+
 driver.findElement(By.name('signin')).click();
-
-
-
+driver.wait(function() {
+    // console.error('wait
+   
+    return driver.findElements(By.name("content"));
+}, 10000).then(re => {
+        console.log('test', Object.keys(re));
+        // re.findElement(By.css('input')).sendKeys('ol2007july@gmail.com')
+    });
+//driver.findElement(By.xpath('//*[@id="user-popup-parent59321"]/div/div')).isSelected();
+driver.findElement(By.css('input')).sendKeys('ol2007july@gmail.com');
 
 
 
